85e82eb7f4a11b180776389b92b98b58b707c90f
jiro <jiroron666@gmail.com>
2018-01-28 19:33:42 +0900

    不正な変数を削除

--- util.go
+++ util.go
@@ -7,17 +7,17 @@ import (
 )
 
 // 設定ファイルの保存先ディレクトリを作成する
-func MkdirConfigDir(appnm string) error {
-	home := getHomeEnv()
+func MkConfigDir(appnm string) error {
+	home := GetEnvHome()
 	// 保存先ディレクトリ
-	dir := filepath.Join(home, dcfg, appnm)
+	dir := filepath.Join(home, appnm)
 	err := os.MkdirAll(dir, os.ModeDir)
 	return err
 }
 
 // 環境変数HOMEを取得するが、
 // HOMEが空で且つ環境がWindowsならAPPDATAを使用する
-func getHomeEnv() string {
+func GetEnvHome() string {
 	home := os.Getenv("HOME")
 	if home == "" && runtime.GOOS == "windows" {
 		// WindowsでHOME環境変数が定義されていない場合
